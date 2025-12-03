5db5a74f064fd7034e3cdbe3dd062e8304fac16b
SankalpHendre <sankalphendre5281@gmail.com>
2025-12-03 12:34:10 +0530

    first commit

---
+++ src/main/java/insurance/cv/CaravanInsuranceApplication.java
@@ -0,0 +1,13 @@
+package insurance.cv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaravanInsuranceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaravanInsuranceApplication.class, args);
+	}
+
+}
