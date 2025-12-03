a4b0f2b04f5bd3dfb4c03bb7633781387ba0c4bd
SankalpHendre <sankalphendre5281@gmail.com>
2025-12-03 13:51:31 +0530

    UserController

--- src/main/java/insurance/cv/controller/UserController.java
+++ src/main/java/insurance/cv/controller/UserController.java
@@ -1,10 +1,12 @@
 package insurance.cv.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
-   public void hello() {
-	   
+	@GetMapping("/")
+   public String hello() {
+	   return "Hello Controller";
    }
 }
