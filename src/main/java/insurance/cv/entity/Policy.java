37ef1f155039db10a642a95197eeb89fb7be1fba
SankalpHendre <sankalphendre5281@gmail.com>
2025-12-03 17:49:25 +0530

    Policy Entity updated

--- src/main/java/insurance/cv/entity/Policy.java
+++ src/main/java/insurance/cv/entity/Policy.java
@@ -1,10 +1,17 @@
 package insurance.cv.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name="policies")
 public class Policy {
-
+@Id
+@GeneratedValue(strategy = GenerationType.AUTO)
+@Column(name = "Policy_id")
+private int id;
 }
